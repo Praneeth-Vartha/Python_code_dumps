db07d17f7713817bed635b7a60805138b5ba5e11
Vartha-Praneeth <saipraneethvartha172@gmail.com>
2022-04-06 12:32:01 +0530

    visual studio cloning

--- email_smtp.py
+++ email_smtp.py
@@ -1,9 +1,9 @@
 #seding mail using SMTP protocol
-import smtplib
+import smtplib 
 mail="varthpraneeth@gmail.com"
 password="machilipatnam"
 connection=smtplib.SMTP("smpt.gmail.com")
 connection.starttls()
 connection.login(user=mail,password=password)
-connection.sendmail(from_addr=mail,to_addr="saipraneethvartha172@gmail.com",msg="python")
+connection.sendmail(from_addr=mail,to_addr="saipraneethvartha172@gmail.com",msg="python visual studio code testing")
 connection.close()
