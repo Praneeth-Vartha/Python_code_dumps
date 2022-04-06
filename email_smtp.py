c3f085588ea35a4dec6610c48267891fda043dd7
Vartha-Praneeth <saipraneethvartha172@gmail.com>
2022-04-06 12:37:32 +0530

    vs code 01

--- email_smtp.py
+++ email_smtp.py
@@ -5,5 +5,5 @@ password="machilipatnam"
 connection=smtplib.SMTP("smpt.gmail.com")
 connection.starttls()
 connection.login(user=mail,password=password)
-connection.sendmail(from_addr=mail,to_addr="saipraneethvartha172@gmail.com",msg="python visual studio code testing")
+connection.sendmail(from_addr=mail,to_addr="saipraneethvartha172@gmail.com",msg="python visual studio code testing 01")
 connection.close()
